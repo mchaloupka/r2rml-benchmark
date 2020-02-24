3ed5b4719caadab0a6730b7699a3b7f70ad40e7d
Milos Chaloupka <chaloupkamilos@hotmail.com>
2020-02-24 22:04:34 +0000

    Reverting to default benchmark configuration

--- main.fsx
+++ main.fsx
@@ -7,5 +7,5 @@ open Benchmark
 // * quickTestBenchmarkConfiguration - a benchmark that uses only the lowest product and client count from the full benchmark
 // * upperBoundaryBenchmarkConfiguration - a benchmark that uses only the highest product and client count from the full benchmark
 // Or you can customize them to perform alternative benchmark
-quickTestBenchmarkConfiguration
+defaultBenchmarkConfiguration
 |> performBenchmark
