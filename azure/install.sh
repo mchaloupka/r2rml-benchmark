4e42fb951fb9fe449601ea6829b3766f42d6141c
Milos Chaloupka <chaloupkamilos@hotmail.com>
2021-04-29 23:25:22 +0100

    Install script and default valus for deployment

--- azure/install.sh
+++ azure/install.sh
@@ -2,3 +2,14 @@
 echo "Prepare for install"
 apt update
 apt upgrade -y
+
+echo "Change directory"
+cd ~/
+
+echo "Install docker"
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+rm get-docker.sh
+
+echo "Pull r2rml-benchmark repository"
+git clone https://github.com/mchaloupka/r2rml-benchmark.git
